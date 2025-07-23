ee2711719baac48a85ac765b4adad476babd9727
merterkoc <mertyasarerkocc@gmail.com>
2025-07-23 17:41:02 +0300

    Add localization support

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:shartflix/shartflix_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ShartflixApp());
 }
