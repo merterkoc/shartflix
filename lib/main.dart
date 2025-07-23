0ad1b50868b5eb8f09c3e4568be6326fbfc62fdc
merterkoc <mertyasarerkocc@gmail.com>
2025-07-23 16:51:59 +0300

    Asset automation (resizer script, flutter_gen), docs and setup improvements

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shartflix/shartflix_app.dart';
 
 void main() {
   runApp(const ShartflixApp());
-}
-
-class ShartflixApp extends StatelessWidget {
-  const ShartflixApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'ShartflixApp', home: Home());
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
 }
