7afa8b9b68bfb06b77c530582e3a74d5b43ee850
mert <mertyasarerkocc@gmail.com>
2025-07-28 01:19:16 +0300

    Splash screen and app icons added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:shartflix/boot.dart';
 import 'package:shartflix/shartflix_app.dart';
 
 Future<void> main() async {
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await BootService.initialize();
+  FlutterNativeSplash.remove();
+  
   runApp(const ShartflixApp());
 }
